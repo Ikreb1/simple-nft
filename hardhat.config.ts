de121f0423002e899f776ecc1461c41f604b2278
Breki <breki@directivegames.com>
2021-12-13 15:01:45 +0000

    playing around with merkletree

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,6 +7,9 @@ import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 
+const SHA256 = require('crypto-js/sha256')
+import { MerkleTree } from 'merkletreejs';
+
 dotenv.config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
@@ -18,6 +21,36 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
+
+task("merkle", "makes a merkel tree", async (taskArgs, hre) =>{
+  /**
+   * https://github.com/miguelmota/merkletreejs-solidity
+   * As is, this implemenation is vulnerable to a second pre-image attack. 
+   * Use a difference hashing function for leaves and nodes, so that H(x) != H'(x). 
+   * Also, as is, this implementation is vulnerable to a forgery attack 
+   * for an unbalanced tree, where the last leaf node can be duplicated to 
+   * create an artificial balanced tree, resulting in the same Merkle root hash. 
+   * Do not accept unbalanced tree to prevent this.
+   * More info here.
+   */
+  const accounts = await hre.ethers.getSigners()
+
+  console.log("unhashed addresses")
+  let count = 0;
+  for (const account of accounts) {
+    console.log(`${count}# ${account.address}`)
+    count++
+  }
+  
+  const leaves = accounts.map(x => SHA256(x.address));
+  const tree = new MerkleTree(leaves, SHA256)
+  const root = tree.getRoot().toString('hex')
+  const leaf = SHA256(accounts[0].address)
+  const proof = tree.getProof(leaf)
+  console.log(MerkleTree.verify(proof, leaf, root))
+
+  console.log(tree.toString())  
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
